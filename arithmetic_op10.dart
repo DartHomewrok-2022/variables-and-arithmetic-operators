21ce5119f0feab04e95c2f77e55c6b309eb4e3aa
Sanjar1218 <71938677+Sanjar1218@users.noreply.github.com>
2022-05-19 10:57:47 +0500

    Update arithmetic_op10.dart

--- arithmetic_op10.dart
+++ arithmetic_op10.dart
@@ -1,11 +1,18 @@
-// Create a variable "n"  and assign it the value of the 4.
+// Create a variable "a"  and assign it the value of the int 8.
+// Create a variable "b"  and assign it the value of the int 5.
+// Create a variable "c"  and assign it the value of the int 2.
+// Create a variable "d"  and assign it the value of the int 6.
 
 // Create a variable called 'answer'.
 
-// Find the expression value assign to 'answer'. 2^n.
+// Find the expression value assign to 'answer'.  
+a*b / c+d .
 
 void main() {
-  int n = 0;
-  var answer = 0 ^ 1;
-  print(answer);
+ int a = 8;
+ int b = 5;
+ int c = 2;
+ int d = 6;
+ double answer = a * b / c + d;
+ print(answer);
 }
